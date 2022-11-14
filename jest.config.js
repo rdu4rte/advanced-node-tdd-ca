ea9664a4a760dc6c91f70186e2c60fb82b1b850d
Raul Duarte <raulduarte94@hotmail.com>
2022-11-14 15:09:14 -0300

    :wrench: setup jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  collectCoverage: false,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageProvider: 'babel',
+  moduleNameMapper: {
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1',
+  },
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  transform: {
+    '\\.ts$': 'ts-jest',
+  },
+};
